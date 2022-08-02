303a1bd9a736234ca6bfd2521088c7bc88209144
mahesh-543 <maheshdevops543@gmail.com>
2022-08-02 03:25:59 +0000

    modified again

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 	public static void main(String args[]) {
 		//prints "hello, world" in the terminal window.
-		system.out.println("hello,world");
+		System.out.println("hello,world");
 	}
 
 
